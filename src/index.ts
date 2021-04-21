fc458aa25e40ee04eec6c33baccea5d8e58275c2
lawrenceagles <lawrenceagles@gmail.com>
2021-04-21 21:15:56 +0100

    finished startr

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
-import fastify from 'fastify';
+import { fastify } from 'fastify';
 import pino from 'pino';
-
 const Port = process.env.PORT || 7000;
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/blogs';
 const server = fastify({
 	logger: pino({ level: 'info' })
 });
 
+// register plugin below:
+
 const start = async () => {
 	try {
 		await server.listen(Port);
-
-		const address = server.server.address();
-		console.log(`Server listening at ${Port}}`);
+		console.log('Server started successfully');
 	} catch (err) {
 		server.log.error(err);
 		process.exit(1);
 	}
 };
 start();
-
-// server.get('/ping', async (request, reply) => {
-//     server.log.info('log message');
-//     return 'pong\n';
-// });
